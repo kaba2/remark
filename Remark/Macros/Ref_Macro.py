4cf99e1b6a6e81c1da6429f67940c879e244f537
Kalle Rutanen <kaba@hilvi.org>
2010-03-10 01:48:27 +0200

    Added the Ref macro.

---
+++ Remark/Macros/Ref_Macro.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Description: Ref_Macro class
+# Detail: Finds the relative output name of a file in document tree.
+
+import os.path
+
+from MacroRegistry import registerMacro
+from Common import linkAddress, outputDocumentName
+
+class Ref_Macro:
+    def expand(self, parameter, remarkConverter):
+        document = remarkConverter.document
+        documentTree = remarkConverter.documentTree
+        
+        text = []
+        
+        for linkFileName in parameter:
+            linkDocument, unique = documentTree.findDocumentHard(linkFileName, document.relativeDirectory)
+            if not unique:
+                remarkConverter.reportWarning('Ref: "' + linkFileName + '" is ambiguous. Picking arbitrarily.')
+            
+            if linkDocument != None:
+                linkTarget = linkAddress(document.relativeDirectory, linkDocument.relativeName)
+                text.append(outputDocumentName(linkTarget))
+                if len(parameter) > 1:                
+                    text += ['']
+            else:
+                remarkConverter.reportWarning('Ref: "' + linkFileName + '" not found. Ignoring it.')
+            
+        return text
+    
+    def outputType(self):
+        return 'remark'
+
+    def pureOutput(self):
+        return True
+
+    def htmlHead(self, remarkConverter):
+        return []                
+
+    def postConversion(self, inputDirectory, outputDirectory):
+        None
+
+registerMacro('Ref', Ref_Macro())
