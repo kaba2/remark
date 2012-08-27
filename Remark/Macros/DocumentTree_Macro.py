ce46964ccb80dbc5e4991cf3908f722276bca1d1
Kalle Rutanen <kaba@hilvi.org>
2012-08-27 03:13:59 +0300

    Generalized DocumentTree filtering to globs and regexes.

--- Remark/Macros/DocumentTree_Macro.py
+++ Remark/Macros/DocumentTree_Macro.py
@@ -3,6 +3,9 @@
 # Description: DocumentTree macro
 # Detail: Generates a Remark file to traverse the document-tree directly. 
 
+import fnmatch
+import re
+
 from MacroRegistry import registerMacro
 from Common import htmlDiv
 
@@ -13,14 +16,25 @@ class DocumentTree_Macro(object):
     def expand(self, parameter, remarkConverter):
         scope = remarkConverter.scopeStack.top()
 
+        # Variables
         self.className = scope.getString('DocumentTree.class_name', 'DocumentTree')
         self.minDepth = scope.getInteger('DocumentTree.min_depth', 1)
         self.maxDepth = scope.getInteger('DocumentTree.max_depth', 10)
-        self.excludeTag = scope.getString('DocumentTree.exclude_tag', 'document_type')
-        self.excludeSet = scope.get('DocumentTree.exclude_set', [''])
+        self.filterTag = scope.getString('DocumentTree.filter_tag', 'document_type')
+        self.filter = scope.getString('DocumentTree.filter', '*', ' ')
+        self.regexFilter = scope.getString('DocumentTree.regex_filter', '', r'\n')
+
+        # Precomputation
         self.remarkConverter = remarkConverter
         self.document = remarkConverter.document
         self.documentTree = remarkConverter.documentTree
+
+        if self.regexFilter != '':
+            # Use the regular expression for filtering.
+            self.filterRegex = re.compile(self.regexFilter)
+        else:
+            # Use the glob for filtering.
+            self.filterRegex = re.compile(fnmatch.translate(self.filter))
 
         # Start reporting the document-tree using the
         # current document as the root document.
@@ -46,9 +60,10 @@ class DocumentTree_Macro(object):
         if depth > self.maxDepth:
             return
 
-        # Exclude those whose tags are in the exclusion set.
-        tag = document.tagString(self.excludeTag, None)
-        if tag in self.excludeSet: 
+        # Only report those documents which match the
+        # filter.
+        tag = document.tagString(self.filterTag, None)
+        if self.filterRegex.match(tag) == None: 
             return
 
         listPrefix = '    ' * (depth - self.minDepth) + ' 1. '
