6f743b8ad6906437e6208cc56424eea508126784
kaba <kaba@localhost>
2012-09-21 00:00:38 +0300

    Added indentation macros. Added Equation and EquationSet macros.

---
+++ Remark/Macros/Equation_Macro.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Description: Equation macro
+# Detail: Embeds the parameter into '' and a div-block.
+
+from MacroRegistry import registerMacro
+
+class Equation_Macro(object):
+    def __init__(self):
+        self.equationNumber = 0
+
+    def name(self):
+        return 'Equation'
+
+    def expand(self, parameter, remark):
+        text = []
+        dependencySet = set()
+
+        text.append('<div class = "equation">')
+        text.append('<span class = "equation-body">' + "''")
+        text += parameter
+        text.append("''" + '</span>')
+        text.append('<span class = "equation-number">' + 
+                    str(self.equationNumber) + 
+                    '</span>')
+        text.append('</div>')
+        self.equationNumber += 1
+
+        return text, dependencySet
+
+    def outputType(self):
+        return 'remark'
+
+    def expandOutput(self):
+        return True
+
+    def htmlHead(self, remark):
+        return []                
+
+    def postConversion(self, inputDirectory, outputDirectory):
+        None
+
+registerMacro('Equation', Equation_Macro())
+
+
+
+
+
