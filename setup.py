da1d933f521929c30fc18aa9c7e6135b317b6713
Kalle Rutanen <kaba@hilvi.org>
2013-05-24 22:38:58 +0200

    Added pip packaging.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(name = 'remark',
+      version = '1.6.0',
+      description = 'Remark',
+      long_description = 'Generates html documentation for software libraries from lightweight markup.',
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Console',
+        'Topic :: Software Development :: Documentation',
+      ],
+      keywords = 'lightweight markup software documentation html',
+      url = 'http://kaba.hilvi.org/remark',
+      author = 'Kalle Rutanen',
+      license = 'MIT',
+      packages = ['Remark'],
+      install_requires = [
+        'markdown == 2.0.0', 
+        'pygments >= 1.5',
+        'pil >= 1.1'
+      ],
+      scripts = {
+          'Remark/remark.py'],
+      },
+      zip_safe=False)
