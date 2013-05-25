de0c724e18b0cbac7052239f215527743c8cf264
kaba <kaba@localhost>
2013-05-25 17:24:19 +0200

    Updated packaging.

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@
 
 from distutils.core import setup
 
-setup(name='remark',
-      version='1.6.0',
-      description='Remark',
+setup(name = 'remark',
+      version = '1.6.0',
+      description = 'Remark',
       long_description = 'Generates html documentation for software libraries from lightweight markup.',
       keywords = 'lightweight markup software documentation html',
-      author='Kalle Rutanen',
-      author_email='kalle_rutanen@hotmail.com',
-      url='http://kaba.hilvi.org/remark',
-      packages=[
+      author = 'Kalle Rutanen',
+      author_email = 'kalle_rutanen@hotmail.com',
+      url = 'http://kaba.hilvi.org/remark',
+      packages = [
         'Remark', 
         'Remark.Macros',
         'Remark.DocumentTypes',
         'Remark.TagParsers',
         ],
       license = 'MIT',
-      classifiers=[
+      classifiers = [
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
@@ -29,8 +29,8 @@ setup(name='remark',
         ],
         scripts = ['Remark/remark.py',],
         requires = [
-          'markdown==2.0.0', 
-          'pygments>=1.5',
-          'pil>=1.1',
+          'markdown (==2.0.0)', 
+          'pygments (>=1.5)',
+          'pil (>=1.1)',
         ],
      )
