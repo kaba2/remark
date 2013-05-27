4189e6be0d0c017c21bf8e9c49d4946b443416f8
kaba <kaba@localhost>
2013-05-27 16:36:44 +0200

    Added missing Version module.

---
+++ Remark/Version.py
@@ -0,0 +1,12 @@
+def remarkVersion():
+    '''
+    Returns the version of Remark as a string.
+    '''
+    return '1.6.0'
+
+def asciiMathMlName():
+    '''
+    Returns the name of the Javascript file to
+    use for AsciiMathML.
+    '''
+    return 'ASCIIMathMLwFallback.js'
