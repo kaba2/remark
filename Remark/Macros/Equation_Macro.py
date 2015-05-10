bfec9b52b8365b93f32a9c29078b0e1496e44ff8
kaba2 <kaba2@localhost>
2015-05-11 01:10:20 +0300

    Removed regions from Equation macro.

--- Remark/Macros/Equation_Macro.py
+++ Remark/Macros/Equation_Macro.py
@@ -15,37 +15,13 @@ class Equation_Macro(object):
         return 'Equation'
 
     def expand(self, parameter, remark):
-        text = []
+        text = parameter
 
-        document = remark.document
+        if len(text) >  0:
+            text[0] = self.leftSymbol + text[0]
+            text[-1] += self.rightSymbol
 
-        # Tags
-        equationNumber = document.tagInteger('Equation.number', None)
-        if equationNumber == None:
-            document.setTag('Equation.number', ['1'])
-            equationNumber = 1
-        document.setTag('Equation.number', [repr(equationNumber + 1)])
-
-        # Variables
-        scope = remark.scopeStack.top()
-        className = scope.getString('Equation.class_name', 'Equation')
-        bodyClassName = scope.getString('Equation.body_class_name', 'Equation-Body')
-        numberClassName = scope.getString('Equation.number_class_name', 'Equation-Number')
-
-        if len(parameter) >  0:
-            parameter[0] = self.leftSymbol + parameter[0]
-            parameter[-1] += self.rightSymbol
-
-        text = markdownRegion(parameter, {'class' : bodyClassName, 'remark-content' : 'remark-no-p'})
-        text += markdownRegion(
-            [str(equationNumber)], 
-            {
-                'class' : numberClassName,
-                'remark-content' : 'text'
-            }, 
-            'span')
-
-        return markdownRegion(text, {'class' : className})
+        return text
 
     def expandOutput(self):
         return False
@@ -57,9 +33,5 @@ class Equation_Macro(object):
         None
 
 registerMacro('Equation', Equation_Macro("''", "''"))
-registerMacro('EquationLatex', Equation_Macro('$', '$'))
-
-
-
-
-
+registerMacro('Equation_Latex', Equation_Macro('$', '$'))
+registerMacro('Equation_Latex_D', Equation_Macro('$$', '$$'))
